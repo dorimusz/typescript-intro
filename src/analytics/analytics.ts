5c9318409ba915e10c3405f3e5f587c0bf57f65f
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-16 11:21:45 +0200

    more tsconfig options

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -1,1 +1,16 @@
 console.log("Sending analytics...");
+let logged;
+
+function sendAnalytics(data: string) {
+  console.log(data);
+  logged = true; //it works w variables but not parameters
+  console.log(logged);
+}
+
+sendAnalytics("the data");
+
+/*
+noImplicitAny: true - tells the compiler that every variable must have a type
+strictNullChecks: false - tells the compiler that it should allow undefined or null (button element)
+strictNullChecks: true - tells the compiler that it should not allow undefined or null
+*/
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 const button = document.querySelector("button")!;
+// ! cheap workaround for nullable types
 
-button.addEventListener("click", () => {
-  console.log("Clicked");
-});
+function clickHandler(message: string) {
+  console.log("Clicked" + message);
+}
+
+// if statement makes sure that the button is not null
+if (button) {
+  // strictBind - false  Won't throw an error if the function is not bound to the button
+  button.addEventListener("click", clickHandler.bind(null, "you're welcome"));
+}
