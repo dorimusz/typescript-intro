50bea8b519c677c43459260652d51fc18fc7b528
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-16 10:10:37 +0200

    rename variables

--- core-types/basics.ts
+++ core-types/basics.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function addNums(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //     throw new Error('Incorrect type');
   // }
@@ -12,7 +12,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 
 const number1 = 5;
 const number2 = 2.8;
-const printResult = true;
+const showResult = true;
 const resultPhrase = "Result is: ";
 
-add(number1, number2, printResult, resultPhrase);
+addNums(number1, number2, showResult, resultPhrase);
